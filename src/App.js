461db1d6c21868b7932b12eb1772b3350e97147d
April <bashi.aquino@gmail.com>
2021-03-02 00:49:42 +0000

    added colorHelper.js with chroma

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, {Component} from 'react';
 import Palette from './Palette';
 import seedColors from "./seedColors";
+import {generatePalette} from './colorHelpers'
 
-function App() {
+class App extends Component {
+
+ render(){
+ console.log(generatePalette(seedColors[4]))
   return (
     <div className="App">
       <Palette {...seedColors[5]}/>
     </div>
   );
+ }
 }
 
 export default App;
